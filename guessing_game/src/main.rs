53c0daf794025870fd42b71bdd029ce5c8824894
Giuseppe Turturiello <giuseppe-pio@hotmail.de>
2024-12-29 15:46:34 +0100

    feat: finish chapter 2 of rust book (guessing game)

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,15 +1,41 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    let mut guess = String::new();
+    loop {
+        let key_to_quit = String::from("quit");
+        println!("Please input your guess. (type '{key_to_quit}' to exit)");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new();
 
-    println!("You guessed: {}", guess);
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        if guess.trim().cmp(&key_to_quit) == Ordering::Equal {
+            println!("Thanks for playing; enjoy your day.");
+            break;
+        }
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }
+    }
 }
